8f94bdf51d421ee123a6c5bf3ea69c4e2ad6db3e
Tyler Brown <tyler.brown@telus.com>
2022-11-24 12:12:15 -0700

    updating breedfetcher repo

--- breedFetcher.js
+++ breedFetcher.js
@@ -1,21 +1,23 @@
 const request = require("request");
-let args = process.argv.slice(2);
 
-request(`https://api.thecatapi.com/v1/breeds/search?q=${args}`, (error, response, body) => {
+const fetchBreedDescription = function(breedName, callback) {
 
-  const data = JSON.parse(body);
+  request(`https://api.thecatapi.com/v1/breeds/search?q=${breedName}`, (error, response, body) => {
 
-  if (error) {
-    console.log(error);
-    process.exit();
-  }
+    const data = JSON.parse(body);
 
-  if (data.length === 0) {
-    {
-      console.log("Breed not found");
-      process.exit();
+    if (data.length === 0) {
+      {
+        console.log("Breed not found");
+        process.exit();
+      }
     }
-  }
-  console.log(data[0].description);
-});
+    console.log(data[0].description);
+  });
+};
+
+module.exports = {
+  fetchBreedDescription
+};
+
 
